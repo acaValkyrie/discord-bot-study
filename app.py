ba663745958d7a3c9a186ec6883890b5ce93028f
acaValkyrie <acavalkyrie@gmail.com>
2024-02-09 19:45:58 +0900

    Add new commands to MyClient class

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
 import discord
 from bot_config import TOKEN, INTENTS
 import modules.message_reply as message_reply
-from discord import Intents, Client, Interaction
+from discord import Intents, Client, Interaction, app_commands
 from discord.app_commands import CommandTree
+from modules.commands import Response
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents,) -> None:
         super().__init__(intents=intents)
         self.tree = CommandTree(self)
+        self.tree.add_command(Response('response'))
 
+    # 実装されたコマンドをBotが読み込む
     async def setup_hook(self) -> None:
         await self.tree.sync()
     
     async def on_ready(self):
-        print('logged in')
+        print('Bot is ready.')
 
     async def on_message(self, message):
         # ignore messages from bot
         if message.author.bot == True: return
         await message_reply.arm(message)
 
-client = MyClient(intents=INTENTS)
-
-@client.tree.command(name="hello", description="挨拶は大事")
-async def test_command_definition(interaction: discord.Interaction):
-    await interaction.response.send_message('hello')
-
 def main():
+    client = MyClient(intents=INTENTS)
     client.run(TOKEN)
 
 if __name__ == '__main__':
--- modules/commands.py
+++ modules/commands.py
@@ -1,6 +1,14 @@
 import discord
-from app import client
+from discord import app_commands
 
-@client.tree.command(name="hello", description="挨拶は大事")
-async def test_command_definition(interaction: discord.Interaction):
-    await interaction.response.send_message('hello')
+class Response(app_commands.Group):
+    def __init__(self, name: str):
+        super().__init__(name=name)
+    
+    @app_commands.command(name="hello", description="挨拶は大事")
+    async def hello_command(self, interaction: discord.Interaction):
+        await interaction.response.send_message('hello')
+
+    @app_commands.command(name="call_shanks", description="シャンクスを呼びます")
+    async def sanx_command(self, interaction: discord.Interaction):
+        await interaction.response.send_message('この戦争を終わらせに来た!!!')
